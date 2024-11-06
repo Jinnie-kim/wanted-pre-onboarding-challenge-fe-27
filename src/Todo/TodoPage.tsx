37d5157aafd23417bb7915cfef55b5e51aaef564
Jinnie <hjk7639@gmail.com>
2024-11-06 21:46:37 +0900

    Add: TodoPage

---
+++ src/Todo/TodoPage.tsx
@@ -0,0 +1,13 @@
+import TodoForm from './TodoForm';
+import TodoLists from './TodoLists';
+
+function TodoPage() {
+  return (
+    <>
+      <TodoForm />
+      <TodoLists />
+    </>
+  );
+}
+
+export default TodoPage;
